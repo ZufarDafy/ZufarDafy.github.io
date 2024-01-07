d6050f5d7452fe16dfb8364d75a4b6d80978e554
BerilBerekhya <beril.berekhya@mhs.itenas.ac.id>
2024-01-07 18:52:03 +0700

    Benerin anomali navbar, testing migrate, memindahkan index ke folder luar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,34 +1,32 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">
-      <div class="d-flex align-items-center">
-        <img src="img/alstore.png" alt="Alstore" height="30">
-        <span class="ms-2">Alstore</span>
-      </div>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="d-flex align-items-center">
+      <img src="img/alstore.png" alt="Alstore">
+      <a class="navbar-brand" href="/">Alstore</a>
+    </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarsExample04">
       <ul class="navbar-nav">
-        <li class="nav-item ml-auto">
+        <li class="nav-item">
           <a class="nav-link {{ ($title == "Home") ? 'active' : '' }}" aria-current="page" href="/">Home</a>
         </li>
+        
+        @auth
+        <form action="/logout" method="post">
+          @csrf
+          <button type="submit" class="nav-link {{ ($title == "Login") ? 'active' : '' }}"><div class="title">Log Out</div></button>
+        </form>
+        @else
         <li class="nav-item">
           <a class="nav-link {{ ($title == "Login") ? 'active' : '' }}" href="/login">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link {{ ($title == "Login") ? 'active' : '' }}" href="/login">Registrasi</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
+        @endauth
       </ul>
     </div>
   </div>
